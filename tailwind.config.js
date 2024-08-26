e576ed86ff82ecbb86c3afd3d63f6d6923d34b03
Dhairya <dhairyacn14@gmail.com>
2024-08-26 23:06:17 +0530

    files added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ export default {
       boxShadow: {
         active:
           "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px",
+        "blue-shadow":
+          "rgba(5, 99, 187, 0.25) 0px 54px 55px, rgba(5, 99, 187, 0.12) 0px -12px 30px, rgba(5, 99, 187, 0.12) 0px 4px 6px, rgba(5, 99, 187, 0.17) 0px 12px 13px, rgba(5, 99, 187, 0.09) 0px -3px 5px",
       },
       fontFamily: {
         popins: ["Poppins", "sans-serif"],
@@ -27,4 +29,3 @@ export default {
   },
   plugins: [],
 };
-  
