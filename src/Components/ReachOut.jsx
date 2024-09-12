d9ff03b66fb2a0aa683532c23d2b58d41f93140b
unknown <dhairya.vardhan@zypp.app>
2024-09-12 23:46:49 +0530

    changes added

--- src/Components/ReachOut.jsx
+++ src/Components/ReachOut.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import { IoLocationOutline } from "react-icons/io5";
 
 const data = [
   {
-    logo: "logo1",
-    heading: "Heading1",
+    logo: <IoLocationOutline className="h-6 w-6 text-blue"></IoLocationOutline>,
+    heading: "Location:",
     extraDetails: ["dhairyacn14@gmail.com", "dhairyacn2020@gmail.com"],
   },
 ];
 
 const ReachOut = () => {
   return (
-    <div className="flex flex-col">
-      {data.map((item) => {
+    <div className="flex flex-col mt-[60px]">
+      {data.map((item) => { 
         return (
-          <div className="flex">
-            <div className="bg-[#eef7ff] rounded-full items-center justify-center">{item.logo}</div>
+          <div className="flex justify-center">
+            <div className="bg-[#cde3fa] flex rounded-full items-center justify-center h-12 w-12 mr-4">{item.logo}</div>
             <div>
-              <h1>{item.heading}</h1>
-              <p>{item.extraDetails}</p>
+              <h3 className="text-xl font-semibold text-[#45505b] font-popins">{item.heading}</h3>
+              <p className="text-[#728394] font-popins text-sm">{item.extraDetails}</p>
             </div>
           </div>
         );
